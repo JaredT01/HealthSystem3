c9fb71ee4dfa51f5ab2ec780d4c807685e504857
RoyalGuilds <Jared.trenholm12@gmail.com>
2021-01-18 13:32:34 -0400

    Initial commit

---
+++ HealthSystem3/Player.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystem3
+{
+    class Player
+    {
+        private int health;
+        private int shield;
+        private int lives;
+        public string name;
+        public bool alive = true;
+
+        public Player()
+        {
+            health = 100;
+            name = "You";
+            shield = 100;
+            lives = 3;
+
+        }
+        public void TakeDamage(int damage)
+        {
+            int OriginalDamage = damage;
+            if (damage > 0)
+            {
+                if (shield > 0)
+                {
+                    if (damage > shield)
+                    {
+                        if (damage > shield + health)
+                        {
+                            OriginalDamage = shield + health;
+                        }
+                        damage = damage - shield;
+                        shield = 0;
+                        health = health - damage;
+                    }
+                    else
+                    {
+                        shield = shield - damage;
+                    }
+                       
+                }
+                else
+                {
+                    shield = 0;
+                    if (damage > health)
+                    {
+                        health = 0;
+                        OriginalDamage = health;
+                    }
+                    else
+                    {
+                        health = health - damage;
+                    }
+                
+                }
+                Console.WriteLine(name + " took "+OriginalDamage+" points of damage!");
+                if(health < 0)
+                {
+                    health = 0;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Damage is not a positive integer and therefore does not work.");
+            }
+
+        }
+        public void DisplayStats()
+        {
+            Console.WriteLine("Health: " + health);
+            Console.WriteLine("Shield: " + shield);
+            Console.WriteLine("Lives: " + lives);
+        }
+        public void CheckPlayer()
+        {
+            if(health <= 0)
+            {
+                if (lives > 0)
+                {
+                    Console.WriteLine(name + " died. However, "+ name+" had another life. Lucky.");
+                    lives = lives - 1;
+                    health = 100;
+                    shield = 100;
+                    Console.ReadKey(true);
+                    Program.debugHeal = 5;
+                    Program.debugDamage = 25;
+                }
+                else
+                {
+                    Console.WriteLine(name + " died... permenantly.");
+                    alive = false;
+                    Console.ReadKey(true);
+                }
+            }
+
+        }
+
+        public void Heal(int heal)
+        {
+            if (health + heal > 100)
+            {
+
+            }
+            else
+            {
+                health = health + heal;
+                Console.WriteLine(name + " healed for " + heal + " health.");
+                Console.ReadKey(true);
+            }
+        }
+
+        public void Regenerate(int regen)
+        {
+            if (shield + regen > 100)
+            {
+
+            }
+            else
+            {shield = shield + regen;
+                Console.WriteLine(name + " regenerated for " + regen + " shield points.");
+                Console.ReadKey(true);
+            }
+        }
+
+    }
+}
---
+++ HealthSystem3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthSystem3
+{
+    class Program
+    {
+         static Player user = new Player();
+        public static int debugDamage = 0;
+        public static int debugHeal = 0;
+        static void Main(string[] args)
+        {
+            for(int x = 0; x < 1;)
+            {
+                if (user.alive == true)
+                {
+                    GameLoop();
+                }
+                else
+                {
+                    Console.ReadKey(true);
+                    x = 1;
+                }
+
+            }
+
+        }
+        static void GameLoop()
+        {
+            debugDamage = debugDamage + 25;
+            debugHeal = debugHeal + 5;
+            Console.Clear();
+            user.DisplayStats();
+            Console.ReadKey(true);
+            user.TakeDamage(debugDamage);
+            Console.ReadKey(true);
+            Console.Clear();
+            user.DisplayStats();
+            user.CheckPlayer();
+
+
+            Console.Clear();
+            user.DisplayStats();
+            user.Heal(debugHeal);
+
+
+            Console.Clear();
+            user.DisplayStats();
+            Console.ReadKey(true);
+            user.Regenerate(debugHeal);
+            
+
+
+        }
+    }
+}
